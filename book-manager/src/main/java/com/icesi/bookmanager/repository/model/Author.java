0771a88e3b2b18a2115bd2d442725008504a03ff
Cristian Perafan <1110282139@u.icesi.edu.co>
2023-11-19 22:05:08 -0500

    Add jpa entities definitions

---
+++ book-manager/src/main/java/com/icesi/bookmanager/repository/model/Author.java
@@ -0,0 +1,25 @@
+package com.icesi.bookmanager.repository.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String nationality;
+}
---
+++ book-manager/src/main/java/com/icesi/bookmanager/repository/model/Book.java
@@ -0,0 +1,24 @@
+package com.icesi.bookmanager.repository.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private Date publicationDate;
+    @ManyToOne
+    private Author author;
+}
